1715387344e2bfd0ee7aae7a35ee1d2bf138585c
Noorul1365 <noorulk135@gmail.com>
2025-11-06 18:50:57 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const redisClient = require("./config/redis");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("E-Commerce CRUD API Runninghhh 🚀"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
